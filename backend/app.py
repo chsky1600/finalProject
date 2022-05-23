10bc219ed3598b87b7e6af092dcb9fe6d3635f4c
chsky1600 <ethanchankowsky@gmail.com>
2022-05-23 18:30:16 -0400

    Heroku Push

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+from flask_restful import Api, Resource, reqparse
+from backend.src.HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='../frontend/build')
+#CORS(app)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", defaults={"path":""})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(HelloApiHandler, '/flask/hello')
